da4744fbb2373453436e647eaa2bdce5f2aac200
Baek Gi Chan <laphayen@gmail.com>
2025-03-06 17:45:30 +0900

    #1 - Disable default DataSource auto-configuration
    Added `@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})` to disable Spring Boot's default database auto-configuration.

--- src/main/java/com/laphayen/loan/LoanApplication.java
+++ src/main/java/com/laphayen/loan/LoanApplication.java
@@ -2,8 +2,9 @@ package com.laphayen.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LoanApplication {
 
 	public static void main(String[] args) {
