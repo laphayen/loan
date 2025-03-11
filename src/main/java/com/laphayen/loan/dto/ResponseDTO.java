98fbf36f500f45f57bb8179902286c742a74e795
Baek Gi Chan <laphayen@gmail.com>
2025-03-11 17:34:32 +0900

    #3 - Add ResponseDTO and ResultObject for standardized API responses
    Added `ResponseDTO` to standardize API response structure with `result` and `data` fields
    Implemented helper methods (`ok()`, `response()`) for success responses
    Added `ResultObject` to handle response status codes and descriptions
    Integrated `BaseException` handling for error responses

---
+++ src/main/java/com/laphayen/loan/dto/ResponseDTO.java
@@ -0,0 +1,45 @@
+package com.laphayen.loan.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.laphayen.loan.exception.BaseException;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude
+public class ResponseDTO<T> implements Serializable {
+
+    private ResultObject result;
+
+    private T data;
+
+    public ResponseDTO(ResultObject result) {
+        this.result = result;
+    }
+
+    public ResponseDTO(T data) {
+        this.data = data;
+    }
+
+    public static <T> ResponseDTO<T> ok() {
+        return new ResponseDTO<>(ResultObject.getSuccess());
+    }
+
+    public static <T> ResponseDTO<T> ok(T data) {
+        return new ResponseDTO<>(ResultObject.getSuccess(), data);
+    }
+
+    public static <T> ResponseDTO<T> response(T data) {
+        return new ResponseDTO<>(ResultObject.getSuccess(), data);
+    }
+
+    public ResponseDTO(BaseException e) {
+        this.result = new ResultObject(e);
+    }
+
+}
