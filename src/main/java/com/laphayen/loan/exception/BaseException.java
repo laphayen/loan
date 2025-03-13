f1acde93de7ca716c1601e56a9cfb6ae7188e76e
Baek Gi Chan <laphayen@gmail.com>
2025-03-13 14:53:08 +0900

    #5 - Extend BaseException from RuntimeException for unchecked exceptions
    Updated `BaseException` to extend `RuntimeException`, allowing unchecked exceptions.

--- src/main/java/com/laphayen/loan/exception/BaseException.java
+++ src/main/java/com/laphayen/loan/exception/BaseException.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class BaseException {
+public class BaseException extends RuntimeException {
 
     private String code="";
     private String description="";
