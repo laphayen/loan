76d8fbf5677011599455691a8619a0b17e515fd4
Baek Gi Chan <laphayen@gmail.com>
2025-03-14 23:59:01 +0900

    #7 - Add ApplicationRepository for loan application database operations
    Created `ApplicationRepository` extending `JpaRepository` for `Application` entity.
    Enables CRUD operations on loan applications.
    Marked with `@Repository` for Spring component scanning.

---
+++ src/main/java/com/laphayen/loan/repository/ApplicationRepository.java
@@ -0,0 +1,10 @@
+package com.laphayen.loan.repository;
+
+import com.laphayen.loan.domain.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+
+}
