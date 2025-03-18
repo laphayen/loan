c1caf6ad96431e7fb51b17df7e8982f34f7cbf60
Baek Gi Chan <laphayen@gmail.com>
2025-03-18 12:18:04 +0900

    #7 - Add endpoints for retrieving loan application data
    Added `GET /applications` to retrieve all loan applications.
    Added `GET /applications/{ApplicationId}` to fetch a specific loan application by ID.

--- src/main/java/com/laphayen/loan/controller/ApplicationController.java
+++ src/main/java/com/laphayen/loan/controller/ApplicationController.java
@@ -4,10 +4,9 @@ import com.laphayen.loan.dto.ApplicationDTO;
 import com.laphayen.loan.dto.ResponseDTO;
 import com.laphayen.loan.service.ApplicationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,9 +15,22 @@ public class ApplicationController extends AbstractController {
 
     private final ApplicationService applicationService;
 
+    // 대출 신청 등록
     @PostMapping
     public ResponseDTO<ApplicationDTO.Response> create(@RequestBody ApplicationDTO.Request request) {
         return ok(applicationService.create(request));
+    }
+
+    // 대출 신청 전체 조회
+    @GetMapping
+    public ResponseDTO<List<ApplicationDTO.Response>> list() {
+        return ok(applicationService.getlist());
+    }
+
+    // 대출 신청 아이디 조회 - 단일
+    @GetMapping("/{ApplicationId}")
+    public ResponseDTO<ApplicationDTO.Response> get(@PathVariable Long ApplicationId) {
+        return ok(applicationService.get(ApplicationId));
     }
 
 
